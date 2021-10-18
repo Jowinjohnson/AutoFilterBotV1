acb4d60e46bd740746fe6637dbd5de31cefcd449
Jowinjohnson <89179002+Jowinjohnson@users.noreply.github.com>
2021-10-18 09:42:55 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -83,7 +83,7 @@ async def auto_filter(bot: Client, update: Message):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}{file_name}"
+            button_text = f"{file_size} - 📂{file_name}"
             
             
             if file_type == "video":
@@ -149,7 +149,7 @@ async def auto_filter(bot: Client, update: Message):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("🔰Go for the Next page⏩", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("💢Click here to view next page💢", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
