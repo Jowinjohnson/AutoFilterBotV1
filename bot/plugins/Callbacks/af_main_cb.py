58ca8b0c94c26ef69876033852334bc0f2afd249
Jowinjohnson <89179002+Jowinjohnson@users.noreply.github.com>
2021-10-18 09:49:36 +0530

    Update af_main_cb.py

--- bot/plugins/Callbacks/af_main_cb.py
+++ bot/plugins/Callbacks/af_main_cb.py
@@ -139,7 +139,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"<i>pls Join our channels Found</i> <code>{leng}</code> <i>Results For Your Request 🤗:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
